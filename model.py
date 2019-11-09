fe069c27dcd82f40b0989b5e4a88795d84cb564f
Joe Cummings <jrcummings27@gmail.com>
2019-11-09 16:29:56 -0600

    joe = god

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from newspaper import Article
+import configparser
+
 import numpy as np
 import psycopg2
 import twitter
+from newspaper import Article
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+config = configparser.ConfigParser()
+config.readfp(open(r'auth.txt'))
 
 # class Recommendation(object):
 #     def __init__(self, title, url, similarity_index):
@@ -13,11 +18,15 @@ import twitter
 
 def run_news_model(username: str) -> dict:
 
-    conn = psycopg2.connect(host="rec-eng.c3nw6fvutjah.us-east-1.rds.amazonaws.com",
-                            user="postgres",
-                            port=1234,
-                            password="fuckthegarage")
+    print(config.get("postgres", "host"), config.get("postgres", "user"), int(config.get("postgres", "port")), config.get("postgres", "password"))
+
+    conn = psycopg2.connect(host=config.get("postgres", "host"),
+                            user=config.get("postgres", "user"),
+                            port=config.get("postgres", "port"),
+                            password=config.get("postgres", "password"))
+    print(conn)
     cur = conn.cursor()
+    print("hello")
 
     stoplist = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
                 "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
@@ -30,9 +39,9 @@ def run_news_model(username: str) -> dict:
                 "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not",
                 "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should",
                 "now", "rt", "de", "el", "la", "amp", "#"]
-
+    print(cur)
     cur.execute("SELECT tweets, url FROM tweets")
-
+    print("here")
     urls = list()
     text = list()
     for tweets, url in cur:
@@ -45,11 +54,12 @@ def run_news_model(username: str) -> dict:
     del text
     print(f"Created training set of {training_idf.shape[0]} urls")
 
-    api = twitter.Api(consumer_key='xea26XfYEJV34VPztm8oGfn5E',
-                    consumer_secret='evA1msunGgC5k4JlyhQVNnJl9SSD6QA0iEbc6gkOMHWyszoWVf',
-                    access_token_key='3734537962-0btTrrB2Kb6ATHlRtrS19iLVr0pIdA5XsIaXJFt',
-                    access_token_secret='a3r1w1lgXg0MQiuQ68HSbz3B4V8cEj9Y0nfkGkmBxbLbb',
+    api = twitter.Api(consumer_key=config.get("twitter", "consumer_key"),
+                    consumer_secret=config.get("twitter", "consumer_secret"),
+                    access_token_key=config.get("twitter", "access_token_key"),
+                    access_token_secret=config.get("twitter", "access_token_secret"),
                     tweet_mode='extended')
+    print(api)
 
     def get_tweets(user):
         try:
@@ -60,7 +70,7 @@ def run_news_model(username: str) -> dict:
         except Exception as e:
             print(e)
 
-    user = get_tweets(username) # right here
+    user = get_tweets(username)
 
     query_idf = vectorizer.transform([user])
     similarity = (training_idf * query_idf.T).toarray()
@@ -80,8 +90,8 @@ def run_news_model(username: str) -> dict:
     print(urls[index])
     print(article.title)
 
-    return {
+    return [{
         "article": article.title, 
         "url": urls[index], 
         "similarity": similarity[index][0]
-    }
+    }]
